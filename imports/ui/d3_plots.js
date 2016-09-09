d7ac30cfb50db00e28e517f5592ff81129087f65
Ian McDonough <immcdonough@ua.edu>
2016-09-08 21:35:06 -0700

    brk: before we add merge_ucsf_dev from akeshavan/mindcontrol

--- imports/ui/d3_plots.js
+++ imports/ui/d3_plots.js
@@ -1,5 +1,63 @@
 import "./module_templates.js"
 
+do_scatter = function(result, dom_id){
+    console.log("you ran the do_scatter function")	
+	console.log($("#chart svg"))
+	
+
+	var result = []
+	_.defer(function () {
+		
+		
+		var chart;
+		    nv.addGraph(function() {
+		        chart = nv.models.scatterChart()
+		            .showDistX(true)
+		            .showDistY(true)
+		            .duration(300)
+		            .color(d3.scale.category10().range());
+		        chart.dispatch.on('renderEnd', function(){
+		            console.log('render complete');
+		        });
+		        chart.xAxis.tickFormat(d3.format('.02f'));
+		        chart.yAxis.tickFormat(d3.format('.02f'));
+		        d3.select('#test1 svg')
+		            .datum(nv.log(randomData(4,40)))
+		            .call(chart);
+		        nv.utils.windowResize(chart.update);
+		        chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
+		        return chart;
+		    });
+		    function randomData(groups, points) { //# groups,# points per group
+		        var data = [],
+		            shapes = ['circle'],
+		            random = d3.random.normal();
+		        for (i = 0; i < groups; i++) {
+		            data.push({
+		                key: 'Group ' + i,
+		                values: [],
+		                slope: Math.random() - .01,
+		                intercept: Math.random() - .5
+		            });
+		            for (j = 0; j < points; j++) {
+		                data[i].values.push({
+		                    x: random(),
+		                    y: random(),
+		                    size: Math.random(),
+		                    shape: shapes[j % shapes.length]
+		                });
+		            }
+		        }
+		        return data;
+		    }
+		
+		
+		
+	})
+	
+}
+
+
 do_d3_date_histogram = function (result, dom_id) {
     // Defer to make sure we manipulate DOM
     _.defer(function () {
